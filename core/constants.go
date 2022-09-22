5a1f355a91842399ac8f6ff6018f3d7e8c050e8c
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-22 14:56:36 -0300

    constants

---
+++ core/constants.go
@@ -0,0 +1,12 @@
+package core
+
+const (
+	KLV = "KLV"
+	KFI = "KFI"
+
+	HRP      = "klv"
+	HDPrefix = "m/44'/690'/0'/0'"
+)
+
+// MaxLenghtOfContracts defines max length of contracts allowed
+const MaxLenghtOfContracts = 20
