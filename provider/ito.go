bc39fc288a7604255628f0764d17e265c7626255
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-23 09:07:30 -0300

    market actions

--- provider/ito.go
+++ provider/ito.go
@@ -6,7 +6,7 @@ import (
 	"github.com/klever-io/klever-go-sdk/models"
 )
 
-func (kc *kleverChain) ConfigITO(base *models.BaseTX, kdaID, receiverAddress string, status int32, maxAmount float64, packs []models.ParsedPack, message string) (*models.Transaction, error) {
+func (kc *kleverChain) ConfigITO(base *models.BaseTX, kdaID, receiverAddress string, status int32, maxAmount float64, packs []models.ParsedPack) (*models.Transaction, error) {
 	kda, err := kc.GetAsset(kdaID)
 	if err != nil {
 		return nil, err
@@ -40,6 +40,43 @@ func (kc *kleverChain) ConfigITO(base *models.BaseTX, kdaID, receiverAddress str
 	}
 
 	data, err := kc.buildRequest(models.TXContract_ConfigITOContractType, base, []interface{}{configITO})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
+
+func (kc *kleverChain) SetITOPrices(base *models.BaseTX, kdaID string, packs []models.ParsedPack) (*models.Transaction, error) {
+	kda, err := kc.GetAsset(kdaID)
+	if err != nil {
+		return nil, err
+	}
+
+	packInfo := make(map[string]models.PackInfoRequest)
+	for _, p := range packs {
+		packPrecision, err := kc.getPrecision(p.Kda)
+		if err != nil {
+			return nil, err
+		}
+
+		packItems := make([]models.PackItemRequest, 0)
+		for _, pItem := range p.Packs {
+			parsedItemAmount := pItem.Amount * math.Pow10(int(kda.Precision))
+			parsedItemPrice := pItem.Price * math.Pow10(int(packPrecision))
+
+			packItems = append(packItems, models.PackItemRequest{Amount: int64(parsedItemAmount), Price: int64(parsedItemPrice)})
+		}
+
+		packInfo[p.Kda] = models.PackInfoRequest{Packs: packItems}
+	}
+
+	setITOPrices := models.SetITOPricesTXRequest{
+		KDA:      kdaID,
+		PackInfo: packInfo,
+	}
+
+	data, err := kc.buildRequest(models.TXContract_SetITOPricesContractType, base, []interface{}{setITOPrices})
 	if err != nil {
 		return nil, err
 	}
---
+++ provider/market.go
@@ -0,0 +1,107 @@
+package provider
+
+import (
+	"math"
+
+	"github.com/klever-io/klever-go-sdk/models"
+)
+
+func (kc *kleverChain) CreateMarketplace(base *models.BaseTX, name, referralAddr string, referralPercent float64) (*models.Transaction, error) {
+	parsedReferralPercent := referralPercent * math.Pow10(2)
+
+	createMarketplace := models.CreateMarketplaceTXRequest{
+		Name:               name,
+		ReferralAddress:    referralAddr,
+		ReferralPercentage: uint32(parsedReferralPercent),
+	}
+
+	data, err := kc.buildRequest(models.TXContract_CreateMarketplaceContractType, base, []interface{}{createMarketplace})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
+
+func (kc *kleverChain) ConfigMarketplace(base *models.BaseTX, id, name, referralAddr string, referralPercent float64) (*models.Transaction, error) {
+	parsedReferralPercent := referralPercent * math.Pow10(2)
+
+	configMarketplace := models.ConfigMarketplaceTXRequest{
+		MarketplaceID:      id,
+		Name:               name,
+		ReferralAddress:    referralAddr,
+		ReferralPercentage: uint32(parsedReferralPercent),
+	}
+
+	data, err := kc.buildRequest(models.TXContract_ConfigMarketplaceContractType, base, []interface{}{configMarketplace})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
+
+func (kc *kleverChain) BuyOrder(base *models.BaseTX, id, currency string, amount float64, buyType int32) (*models.Transaction, error) {
+	parsedAmount := amount
+
+	precision, err := kc.getPrecision(id)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedAmount = amount * math.Pow10(int(precision))
+
+	buyOrder := models.BuyTXRequest{
+		BuyType:    buyType,
+		ID:         id,
+		CurrencyID: currency,
+		Amount:     int64(parsedAmount),
+	}
+
+	data, err := kc.buildRequest(models.TXContract_BuyContractType, base, []interface{}{buyOrder})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
+
+func (kc *kleverChain) SellOrder(base *models.BaseTX, kdaID, currency, mktID string, price, reservePrice float64, endTime int64, mktType int32, message string) (*models.Transaction, error) {
+	precision, err := kc.getPrecision(currency)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedPrice := price * math.Pow10(int(precision))
+	parsedReservePrice := reservePrice * math.Pow10(int(precision))
+
+	sellOrder := models.SellTXRequest{
+		MarketType:    mktType,
+		MarketplaceID: mktID,
+		AssetID:       kdaID,
+		CurrencyID:    currency,
+		Price:         int64(parsedPrice),
+		ReservePrice:  int64(parsedReservePrice),
+		EndTime:       endTime,
+	}
+
+	data, err := kc.buildRequest(models.TXContract_SellContractType, base, []interface{}{sellOrder})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
+
+func (kc *kleverChain) CancelMarketOrder(base *models.BaseTX, orderID string) (*models.Transaction, error) {
+	cancelMarketOrder := models.CancelMarketOrderTXRequest{
+		OrderID: orderID,
+	}
+
+	data, err := kc.buildRequest(models.TXContract_CancelMarketOrderContractType, base, []interface{}{cancelMarketOrder})
+	if err != nil {
+		return nil, err
+	}
+
+	return kc.PrepareTransaction(data)
+}
