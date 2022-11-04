5c859a17343e97051713eb62e16de5d53dccf1df
Samuel Façanha <samufacanha@gmail.com>
2022-11-04 10:57:07 -0300

    fix: decode contract type, added decode demo (#8)
    * fix: decode contract type, added decode demo
    
    * roll back default
    
    * change demo decode tx to transfer
    
    * added error treatment

---
+++ cmd/demo/decode/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/klever-io/klever-go-sdk/cmd/demo"
+	"github.com/klever-io/klever-go-sdk/models"
+)
+
+func main() {
+
+	accounts, wallets, kc, err := demo.InitWallets()
+	if err != nil {
+		panic(err)
+	}
+
+	base := accounts[0].NewBaseTX()
+	tx, err := kc.MultiTransfer(
+		base,
+		"KLV",
+		[]models.ToAmount{{
+			ToAddress: accounts[1].Address().Bech32(),
+			Amount:    1000,
+		}})
+	if err != nil {
+		panic(err)
+	}
+
+	err = tx.Sign(wallets[0])
+	if err != nil {
+		panic(err)
+	}
+
+	decodedTx, err := kc.Decode(tx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\n\n\nEncodedTX: ", tx)
+	fmt.Println("\n\n\nDecodedTX: ", decodedTx)
+}
