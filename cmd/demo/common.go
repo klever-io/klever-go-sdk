581d1efded39e2e195d0b1450ffccd7ab08f3bac
leonardo5621 <leonardo5621@gmail.com>
2023-12-28 19:09:17 -0300

    Revert http client changes

--- cmd/demo/common.go
+++ cmd/demo/common.go
@@ -1,7 +1,6 @@
 package demo
 
 import (
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -23,7 +22,7 @@ func InitWallets() (
 	wallets = make([]wallet.Wallet, 0)
 
 	net := network.NewNetworkConfig(network.TestNet)
-	httpClient := utils.NewHttpClient(http.Client{Timeout: 10 * time.Second})
+	httpClient := utils.NewHttpClient(10 * time.Second)
 	kc, err = provider.NewKleverChain(net, httpClient)
 	if err != nil {
 		return
