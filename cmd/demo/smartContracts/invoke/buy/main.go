701cf0bbef4f23999a9fdf622e6e115fe9dc5bb1
Emerson Barros <emersonfb99@gmail.com>
2024-03-26 13:06:44 -0300

    docs: adds examples on how to do some SC invokes

---
+++ cmd/demo/smartContracts/invoke/buy/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/klever-io/klever-go-sdk/cmd/demo"
+)
+
+func main() {
+	accounts, wallets, kc, err := demo.InitWallets()
+
+	if err != nil {
+		panic(err)
+	}
+
+	base := accounts[0].NewBaseTX()
+
+	tx, err := kc.InvokeSmartContract(
+		base,
+		"klv1qqqqqqqqqqqqqpgq4f6qkvv34kr50w5lucdn6z0238r7m96nhtxspjw22t",
+		"buy_ticket",
+		map[string]int64{"KLV": int64(10000000)},
+		"demo",
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = tx.Sign(wallets[0])
+	if err != nil {
+		panic(err)
+	}
+
+	hash, err := tx.Broadcast(kc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("TxHash: ", hash)
+}
