733793ca8e493bd52b5bdf70f778f280f43a21b0
Emerson Barros <emersonfb99@gmail.com>
2024-04-04 11:11:20 -0300

    feat: initial decoders for single values

---
+++ cmd/demo/smartContracts/decode/decode.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// _, _, kc, err := demo.InitWallets()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// kc.DecodeSCOutput()
+	test := strings.Split("Winner", "<")
+	fmt.Println(test[0])
+}
