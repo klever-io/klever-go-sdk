90a96e33059c4a39fc36fe94974c21a6912e36fa
Emerson Barros <emersonfb99@gmail.com>
2024-04-08 11:15:48 -0300

    refactor: simplifies signed int decoders

--- provider/scAbiDecoder.go
+++ provider/scAbiDecoder.go
@@ -4,7 +4,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"math"
 	"math/big"
 	"os"
 	"strconv"
@@ -178,12 +177,12 @@ func (a *abiData) decodeString(hexValue string) (*string, error) {
 	return &convertedString, nil
 }
 
-func (a *abiData) decodeUint(hexValue string) (*uint64, error) {
+func (a *abiData) decodeDecimalNumber(hexValue string, bitSize int) (*uint64, error) {
 	// decode List/Option/tuple/variadic will "cut" the original string
 	// value := (*hex)[:length]
 	// *hex = (*hex)[length:]
 
-	uintValue, err := strconv.ParseUint(hexValue, BaseHex, 64)
+	uintValue, err := strconv.ParseUint(hexValue, BaseHex, bitSize)
 	if err != nil {
 		return nil, err
 	}
@@ -192,28 +191,28 @@ func (a *abiData) decodeUint(hexValue string) (*uint64, error) {
 }
 
 func (a *abiData) decodeUint8(hexValue string) (*uint8, error) {
-	targetValue, err := a.decodeUint(hexValue)
+	targetValue, err := a.decodeDecimalNumber(hexValue, 8)
 	uint8Decoded := uint8(*targetValue)
 
 	return &uint8Decoded, err
 }
 
 func (a *abiData) decodeUint16(hexValue string) (*uint16, error) {
-	targetValue, err := a.decodeUint(hexValue)
+	targetValue, err := a.decodeDecimalNumber(hexValue, 16)
 	uint16Decoded := uint16(*targetValue)
 
 	return &uint16Decoded, err
 }
 
 func (a *abiData) decodeUint32(hexValue string) (*uint32, error) {
-	targetValue, err := a.decodeUint(hexValue)
+	targetValue, err := a.decodeDecimalNumber(hexValue, 32)
 	uint32Decoded := uint32(*targetValue)
 
 	return &uint32Decoded, err
 }
 
 func (a *abiData) decodeUint64(hexValue string) (*uint64, error) {
-	return a.decodeUint(hexValue)
+	return a.decodeDecimalNumber(hexValue, 64)
 }
 
 func (a *abiData) decodeBigUint(hexValue string) (*big.Int, error) {
@@ -232,94 +231,54 @@ func (a *abiData) decodeBigUint(hexValue string) (*big.Int, error) {
 func (a *abiData) decodeInt8(hexString string) (*int8, error) {
 	const BitSize = 8
 
-	parsedValue, err := strconv.ParseInt(hexString, BaseHex, BitSize)
+	targetValue, err := a.decodeDecimalNumber(hexString, BitSize)
 	if err != nil {
 		return nil, err
 	}
 
-	if uint8(parsedValue) > math.MaxInt8 {
-		ptrtTrgetValue, err := a.fixIntOverflow(hexString, BitSize)
-		if err != nil {
-			return nil, err
-		}
-
-		targetValue := int8(*ptrtTrgetValue)
-		return &targetValue, nil
-	}
-
-	targetValue := int8(parsedValue)
-	return &targetValue, nil
+	targetValueI8 := int8(*targetValue)
+	return &targetValueI8, nil
 }
 
 func (a *abiData) decodeInt16(hexString string) (*int16, error) {
 	const BitSize = 16
 
-	parsedValue, err := strconv.ParseInt(hexString, BaseHex, BitSize)
+	targetValue, err := a.decodeDecimalNumber(hexString, BitSize)
 	if err != nil {
 		return nil, err
 	}
 
-	if uint16(parsedValue) > math.MaxInt16 {
-		ptrtTrgetValue, err := a.fixIntOverflow(hexString, BitSize)
-		if err != nil {
-			return nil, err
-		}
-
-		targetValue := int16(*ptrtTrgetValue)
-		return &targetValue, nil
-	}
-
-	targetValue := int16(parsedValue)
-	return &targetValue, nil
+	targetValueI32 := int16(*targetValue)
+	return &targetValueI32, nil
 }
 
 func (a *abiData) decodeInt32(hexString string) (*int32, error) {
 	const BitSize = 32
 
-	parsedValue, err := strconv.ParseInt(hexString, BaseHex, BitSize)
+	targetValue, err := a.decodeDecimalNumber(hexString, BitSize)
 	if err != nil {
 		return nil, err
 	}
 
-	if uint32(parsedValue) > math.MaxInt32 {
-		ptrtTrgetValue, err := a.fixIntOverflow(hexString, BitSize)
-		if err != nil {
-			return nil, err
-		}
-
-		targetValue := int32(*ptrtTrgetValue)
-		return &targetValue, nil
-	}
-
-	targetValue := int32(parsedValue)
-	return &targetValue, nil
+	targetValueI32 := int32(*targetValue)
+	return &targetValueI32, nil
 }
 
 func (a *abiData) decodeInt64(hexString string) (*int64, error) {
 	const BitSize = 64
 
-	targetValue, err := strconv.ParseUint(hexString, BaseHex, 64)
+	targetValue, err := a.decodeDecimalNumber(hexString, BitSize)
 	if err != nil {
-		return nil, fmt.Errorf("invalid hex string to decode to int64 %s: %v", hexString, err)
+		return nil, err
 	}
 
-	if targetValue > math.MaxInt64 {
-		ptrtTargetValue, err := a.fixIntOverflow(hexString, BitSize)
-		if err != nil {
-			return nil, err
-		}
-
-		targetValue := int64(*ptrtTargetValue)
-		return &targetValue, nil
-	}
-
-	targetValueI64 := int64(targetValue)
+	targetValueI64 := int64(*targetValue)
 	return &targetValueI64, nil
 }
 
 func (a *abiData) decodeBigInt(hexString string) (*big.Int, error) {
 	targetValue, err := a.decodeStringBigNumber(hexString)
-	// if that function suceeds, then it was a string representin a decimal number
+	// if that function suceeds, then it was a string representing a decimal number
 	if err == nil {
 		return targetValue, nil
 	}
@@ -366,21 +325,4 @@ func (a *abiData) decodeStringBigNumber(hexString string) (*big.Int, error) {
 	}
 
 	return targetValue, nil
-}
-
-func (a *abiData) fixIntOverflow(hexString string, bitSize int) (*int64, error) {
-	if bitSize > 64 {
-		return nil, fmt.Errorf("bitSize too large for uint64")
-	}
-
-	targetValue, err := strconv.ParseUint(hexString, BaseHex, 64)
-	if err != nil {
-		return nil, fmt.Errorf("invalid hex string %s: %v", hexString, err)
-	}
-
-	targetValue -= 1 << bitSize
-
-	targetValueInt64 := int64(targetValue)
-
-	return &targetValueInt64, nil
 }
